d2839423d8803c61992ec0674df01a8a36439773
Javi-1997 <121068091+Javi-1997@users.noreply.github.com>
2023-01-17 13:46:03 -0300

    Delete proyecto_code_47635 directory

--- proyecto_code_47635/estudiantes/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from estudiantes.models import Curso, Estudiante, Profesor, Entregable, Instituto
-
-admin.site.register(Curso)
-admin.site.register(Estudiante)
-admin.site.register(Profesor)
-admin.site.register(Entregable)
